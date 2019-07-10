6ea1d7a592024574a5e1779d0d8bf7ec9205270d
Tinaël <tinael.devresse@mic-belgique.be>
2019-07-10 11:41:44 +0200

    Implement lifting state up

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import { Sidebar, Segment } from 'semantic-ui-react';
+import CustomSidebar from './components/CustomSidebar';
 import CustomMap from './components/CustomMap';
-import Sidebar from './components/Sidebar';
+import ExpandCollapseButton from './components/ExpandCollapseButton';
 
-function App() {
-  return (
-    <div className="container">
-      <Sidebar></Sidebar>
-      <CustomMap>
-      </CustomMap>
-    </div>
-  );
+import './App.css';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleHideClick = this.handleHideClick.bind(this);
+    this.handleShowClick = this.handleShowClick.bind(this);
+    this.handleSidebarHide = this.handleSidebarHide.bind(this);
+    this.state = { visible: true };
+  }
+
+  handleHideClick = () => this.setState({ visible: false })
+  handleShowClick = () => this.setState({ visible: true })
+  handleSidebarHide = () => this.setState({ visible: false })
+
+  render() {
+    return (
+      <Sidebar.Pushable as={Segment}>
+        <ExpandCollapseButton
+          visible={this.state.visible}
+          onHideClick={this.handleHideClick} 
+          onShowClick={this.handleShowClick} 
+        >
+        </ExpandCollapseButton>
+        
+        <CustomSidebar visible={this.state.visible} onSidebarHide={this.handleSidebarHide}></CustomSidebar>
+        
+        <Sidebar.Pusher>
+          <CustomMap center={[4.352440, 50.846480]}></CustomMap>
+        </Sidebar.Pusher>
+      </Sidebar.Pushable>
+    );
+  }
 }
-
-export default App;
