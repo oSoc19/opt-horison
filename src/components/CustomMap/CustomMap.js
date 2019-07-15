9cffd1a4f621a2946ac0b05e00501abe5378ad4f
Tim Baccaert <timbaccaert@protonmail.com>
2019-07-15 11:35:47 +0200

    integrated isochrone code

--- src/components/CustomMap/CustomMap.js
+++ src/components/CustomMap/CustomMap.js
@@ -4,7 +4,7 @@ import ReactMapboxGl from 'react-mapbox-gl';
 import { getAllPointSets } from '../../data/pointsets.js';
 import PoiLayer from '../PoiLayer/PoiLayer.js';
 import IsochroneLayer from '../IsochroneLayer/IsochroneLayer.js';
-import { generateIsochrone, intersect } from '../../iso/isochrone.js';
+import { findoptimum, run, intersect } from '../../iso/isochrone.js';
 
 import './CustomMap.css';
 
@@ -25,58 +25,37 @@ export default class CustomMap extends Component {
 	}
 
     async componentDidMount() {
-        let bosa = {
-            longitude: 4.356331,
-            latitude: 50.860699,
-        };
+        let overlap = await run();
+        let pointSets = getAllPointSets();
+        let polygons = [await run()];
+        let points = [];
 
-        let herman = {
-            longitude: 4.350018,
-            latitude: 50.865685
-        };
+        // console.log(overlap);
 
-        /* let point1 = {
-            longitude: 4.3517103,
-            latitude: 50.8503396
-        };
-
-        let point2 = {
-            longitude: 4.356670,
-            latitude: 50.846207
+        /* for (let coordinateList of overlap.geometry.coordinates) {
+            polygons.push({
+                type: "Feature",
+                properties: {},
+                geometry: {
+                    type: "Polygon",
+                    coordinates: coordinateList
+                }
+            });
         }*/
 
-        let bosaiso = await generateIsochrone(bosa, 15);
-        let hermaniso = await generateIsochrone(herman, 15);
-        let intersection = intersect(bosaiso, hermaniso);
+        console.log("POLYGONS: ", polygons);
 
-        if (intersection.hasOwnProperty('geometry')) {
-            let pointSets = getAllPointSets();
-            let polygons = [];
-            let points = [];
-
-            for (let coordinateList of intersection.geometry.coordinates) {
-                polygons.push({
-                    type: "Feature",
-                    properties: {},
-                    geometry: {
-                        type: "Polygon",
-                        coordinates: coordinateList
-                    }
-                });
-            }
-
-            for (let pointSet of pointSets) {
-                let intersectingPoints = pointSet.intersect({
-                    type: "FeatureCollection",
-                    features: polygons
-                });
-                intersectingPoints.image = pointSet.image;
-                intersectingPoints.name = pointSet.name
-                points.push(intersectingPoints);
-            }
-
-            this.setState({ polygons, points });
+        for (let pointSet of pointSets) {
+            let intersectingPoints = pointSet.intersect({
+                type: "FeatureCollection",
+                features: polygons
+            });
+            intersectingPoints.image = pointSet.image;
+            intersectingPoints.name = pointSet.name
+            points.push(intersectingPoints);
         }
+
+        this.setState({ polygons, points });
     }
  
 	render() {
